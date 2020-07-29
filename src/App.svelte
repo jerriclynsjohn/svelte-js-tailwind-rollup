c62054990ca5b66f55d0af794ba0f804bf76b8d9
Jerric Lyns John <jerriclynsjohn@outlook.com>
2020-07-29 23:13:06 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import './assets/styles/main.css';
+	export let name;
+</script>
+
+<style>
+	.center {
+		@apply flex justify-center;
+	}
+</style>
+
+<main>
+	<div class="flex justify-center">
+		<h1 class="text-2xl text-gray-700">Hello {name}!</h1>
+	</div>
+	<div class="center">
+		<p class="text-gray-700 text-xl">Visit the <a class="text-blue-600" href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</div>
+</main>
